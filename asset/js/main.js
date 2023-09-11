4c6751fd5d600aa6c30554305a924bf80e6a4f90
danielplebani <danielpleba03@gmail.com>
2023-09-11 13:31:29 +0200

    bonus img

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 
 Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percorso in una propietá dell'oggetto restituito dalla funzione data.
 */
 
 const { createApp } = Vue
@@ -10,7 +10,8 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      message: 'HELLO WORLD'
+      message: 'HELLO WORLD',
+      image: './asset/img/Panda-770x470.jpg'
     }
   }
 }).mount('#app')
