db95ffb9f3454d2f4ca0ef39d689165b697c6c2c
danielplebani <danielpleba03@gmail.com>
2023-09-11 13:09:33 +0200

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+/*
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+    }
+  },
+
+  methods:{  
+  }
+}).mount('#app');
